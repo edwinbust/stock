d5bc47a08f40630c67e053633c4c7ef31357652b
ED BUSAN <ewb2021@gmail.com>
2024-10-27 22:44:03 -0300

    operation

--- data/database_test.sql
+++ data/database_test.sql
@@ -6,6 +6,9 @@ INSERT INTO users (id, username, email, password, role, status, photo) VALUES ('
 INSERT INTO users (id, username, email, password, role, status, photo) VALUES ('5', 'Ana I', 'ana.i@example.com', '800c4664c60e8a48ad4d3ab96039f6e73d423c9ff3', 'user', '1', 'ODAwYzQ2NjRjNjBlOGE0OGFkNGQzYWI5NjAzOWY2ZTczZDQyM2M5ZmYzCgoK');
 INSERT INTO users (id, username, email, password, role, status, photo) VALUES ('6', 'Caty A', 'caty.a@example.com', '4d3ab96039f6e73d423c9ff36303bbb7abef10151b', 'user', '1', 'NGQzYWI5NjAzOWY2ZTczZDQyM2M5ZmYzNjMwM2JiYjdhYmVmMTAxNTFiCgoKCg==');
 
+#TABLA fund_operations
+INSERT INTO fund_operations (id, funde_inv, profit_funde, funde_porc_share, funde_now, timestamp, transactions) VALUES ('1', '1500', '0', '0', '1500', 'Fri Oct 25 10:46:07 -03 2024', '1');
+
 #TABLA fund_transactions
 INSERT INTO fund_transactions (id, user_id, inv_user, operation_id, profit_user, porc_share, share_now, is_latest) VALUES ('1', '1', '0', '1', '0', '0', '0', '1');
 INSERT INTO fund_transactions (id, user_id, inv_user, operation_id, profit_user, porc_share, share_now, is_latest) VALUES ('2', '2', '500', '1', '0', '33.33', '500', '1');
